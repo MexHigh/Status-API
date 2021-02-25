ad7d1ddced0650a9de7c512998835950f114f158
Leon Schmidt <mail@leon.wtf>
2021-02-25 12:09:42 +0100

    Implemeted archive schedule

--- server/schedules/triggers.go
+++ server/schedules/triggers.go
@@ -10,8 +10,27 @@ import (
 // ArchiveTriggerRoutine starts the goroutine schedule
 // to trigger regular archiving
 func ArchiveTriggerRoutine(config *structs.Config) {
-	// TODO add actual scheduling
-	runArchiving(config)
+
+	time.Sleep(time.Duration(2) * time.Second) // Defer start
+
+	nextMidnight := func() time.Time {
+		now := time.Now()
+		return time.Date(now.Year(), now.Month(), now.Day()+1, 0, 3, 0, 0, now.Location())
+	}
+
+	for {
+
+		midnight := nextMidnight()
+		log.Println("Next archiving scheduled at", midnight)
+
+		time.Sleep(time.Until(midnight))
+
+		log.Println("Archiving trigger fired. Running archiving")
+		runArchiving(config)
+		log.Println("Archiving done")
+
+	}
+
 }
 
 const checkIntervalDefault = 120 // two minutes
