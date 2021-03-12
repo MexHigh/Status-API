218ef6be75b118c2da31695d7b65547a4bd4adb9
Leon Schmidt <mail@leon.wtf>
2021-03-12 21:28:40 +0100

    Moved Checker interface to protocols package

--- server/protocols/register.go
+++ server/protocols/register.go
@@ -4,25 +4,30 @@ import (
 	"status-api/structs"
 )
 
+// Checker defines a struct that can perform protocol-specific checks
+type Checker interface {
+	Check(name string, config *structs.ServiceConfig) (structs.CheckResult, error)
+}
+
 // checkers will be filled by protocols.Register()
 // called from within a checker to register itself
-var checkers = make(map[string]structs.Checker)
+var checkers = make(map[string]Checker)
 
 // Register registers a new protocol checker.
 // A registered checker can be used by adding it's
 // name to the "protocol" key in the config.
-// The checker struct must implement the structs.Checker
+// The checker struct must implement the Checker
 // interface and return a CheckResult and an error.
 // If errors are not handled inside the Check() method and
 // are returned, the service will be flagged as down and the
 // error description is passed to the response in the "reason" key.
-func Register(name string, checker structs.Checker) {
+func Register(name string, checker Checker) {
 	checkers[name] = checker
 }
 
 // GetChecker returns the registered checkers, or nil,
 // if it does not exist or has not been registered yet
-func GetChecker(protocol string) structs.Checker {
+func GetChecker(protocol string) Checker {
 	if c, ok := checkers[protocol]; ok {
 		return c
 	}
--- server/structs/checker_if.go
+++
@@ -1,6 +0,0 @@
-package structs
-
-// Checker defines a struct that can perform protocol-specific checks
-type Checker interface {
-	Check(name string, config *ServiceConfig) (CheckResult, error)
-}
