d888fd357deeb615f5001bd678b48e7d697a4aa5
Leon Schmidt <mail@leon.wtf>
2021-03-15 22:15:25 +0100

    Adjusted manifest, made some tests

--- frontend/src/container/ServicesContainer.js
+++ frontend/src/container/ServicesContainer.js
@@ -1,14 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import Loading from "../components/Loading"
 import ServiceContainer from "./ServiceContainer"
 
 // ServicesContainer bundles multiple ServiceContainer components.
 // The whole fetch magic should propably happen in here.
 export default function ServicesContainer() {
-    return (
-        <>
-            <ServiceContainer />
-            <p className="text-red-500">Nadine was here</p>
-        </>
 
-    )
+    const [ data, setData ] = useState()
+
+    useEffect(() => {
+/*
+        fetch("https://status.leon.wtf/api/services/latest")
+            .then(r => r.json())
+            .then(r => {
+                setData(r)
+            })
+*/
+    }, [])
+
+    if (!data) {
+        return <Loading />
+    }
+    else {
+        return (
+            <div>
+                <h1>Last check: {data.at}</h1>
+                {
+                    Object.entries(data.services).map(([key, value]) => 
+                        <ServiceContainer key={key} service={key} />
+                    )
+                }
+            </div>
+        )
+    }
+
 }
