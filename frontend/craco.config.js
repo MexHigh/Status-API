0459fd1b53dfb28aff47d21f8198196bc2b6c96b
Leon Schmidt <mail@leon.wtf>
2021-03-19 23:47:04 +0100

    Added CSP wp plugin, adjusted index.html

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,7 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin')
+
 module.exports = {
 	style: {
 		postcss: {
 			plugins: [require("tailwindcss"), require("autoprefixer")],
 		},
 	},
+	webpack: {
+		plugins: [
+			new HtmlWebpackPlugin(),
+			new CspHtmlWebpackPlugin({
+					"default-src": "'self'",
+					"img-src": "*"
+			})
+		]
+	}
 }
