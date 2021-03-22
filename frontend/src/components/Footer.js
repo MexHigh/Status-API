24de8e858d8e3e388b4bad4586b01339914875d0
Leon Schmidt <mail@leon.wtf>
2021-03-22 20:29:06 +0100

    Added dynamic cyopright year to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<footer className="text-center p-16">
 			<p className="text-gray-800">
-				&copy; 2021 Leon Schmidt at <span className="font-bold">leon.wtf</span>
+				&copy; {new Date().getFullYear()} Leon Schmidt at <span className="font-bold">leon.wtf</span>
 			</p>
 		</footer>
 	)
