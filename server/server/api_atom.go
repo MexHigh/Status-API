ea789e5826df80bb2a4f60a59503ef8343fb3bc4
Leon Schmidt <mail@leon.wtf>
2022-11-12 15:00:00 +0000

    Some changes: - Implemented correct feed update time - Fixed pointer errors while displaying feed - Added Resolved/Unresolved filters - Implemented change and create handlers

--- server/server/api_atom.go
+++ server/server/api_atom.go
@@ -1,17 +1,22 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
+
 	"status-api/database"
 	"status-api/structs"
-	"time"
 
 	"github.com/gorilla/feeds"
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
+
+var feedCreatedUpdated = time.Now()
 
 func rssShowHandler(w http.ResponseWriter, r *http.Request) {
 	feed := &feeds.Feed{
@@ -19,20 +24,24 @@ func rssShowHandler(w http.ResponseWriter, r *http.Request) {
 		Link:        &feeds.Link{Href: "https://leon.wtf"},
 		Description: "Messages about incidents, maintenance, and others for the services running on leon.wtf",
 		Author:      &feeds.Author{Name: "Leon Schmidt", Email: "admin@leon.wtf"},
-		Created:     time.Now(),
+		Created:     feedCreatedUpdated,
 	}
 
 	var items []structs.AtomFeedItemModel
 	database.Con.Find(&items)
 
-	for _, item := range items {
+	//feedsToAdd := make([]*feeds.Item, 0)
+	for index, item := range items {
 		// workaround: feed.ToAtom() panics, if there
 		// is no feeds.Link element, so we will add
 		// an empty feeds.Link
 		if item.Data.Link == nil {
-			item.Data.Link = &feeds.Link{}
+			items[index].Data.Link = &feeds.Link{}
 		}
-		feed.Add((*feeds.Item)(&item.Data))
+		// need to address Data via index, otherwise the
+		// pointer would always point to the last item
+		toAdd := &items[index].Data
+		feed.Add((*feeds.Item)(toAdd))
 	}
 
 	atomFeedString, err := feed.ToAtom()
@@ -45,9 +54,29 @@ func rssShowHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(atomFeedString))
 }
 
+const (
+	atomDescriptionStatusResolved   = "Status: RESOLVED"
+	atomDescriptionStatusUnresolved = "Status: UNRESOLVED"
+)
+
 func rssListMessagesHandler(w http.ResponseWriter, r *http.Request) {
 	var items []structs.AtomFeedItemModel
-	database.Con.Find(&items)
+
+	// get "status" parameter
+	query := r.URL.Query()
+	statusParam := query.Get("status")
+	if statusParam == "" { // if not set, retrieve all items
+		database.Con.Find(&items)
+	} else {
+		if strings.ToLower(statusParam) == "resolved" {
+			database.Con.Where("Data LIKE ?", `%"Description":"`+atomDescriptionStatusResolved+`"%`).Find(&items)
+		} else if strings.ToLower(statusParam) == "unresolved" {
+			database.Con.Where("Data LIKE ?", `%"Description":"`+atomDescriptionStatusUnresolved+`"%`).Find(&items)
+		} else { // return error
+			respondError(&w, errors.New("parameter 'status' can only be one of [resolved, unresolved]"), 400)
+			return
+		}
+	}
 
 	// enrich structs.AtomFeedItem with the database ID (for referencing)
 	//
@@ -69,29 +98,124 @@ func rssListMessagesHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func rssCreateMessageHandler(w http.ResponseWriter, r *http.Request) {
+	// decode response body
+	var respBody struct {
+		Title    string
+		Content  string
+		Resolved bool
+	}
+	if err := json.NewDecoder(r.Body).Decode(&respBody); err != nil {
+		respondError(&w, err, 400) // decoding errors means malformed request
+		return
+	}
+
 	newFeedItem := &structs.AtomFeedItemModel{
 		Data: structs.AtomFeedItem{
-			Title:       "penis",
-			Description: "penis2",
-			Created:     time.Now(),
+			Title:   respBody.Title,
+			Content: respBody.Content,
+			Created: time.Now(),
 		},
 	}
-	database.Con.Create(newFeedItem)
+	if respBody.Resolved {
+		newFeedItem.Data.Description = atomDescriptionStatusResolved
+	} else {
+		newFeedItem.Data.Description = atomDescriptionStatusUnresolved
+	}
 
-	respondPlainJSON(&w, []byte(`{"response": "created"}`), 201) // TODO if existent
+	// generate an ID
+	titleTruncated := strings.ReplaceAll(newFeedItem.Data.Title, " ", "")
+	titleTruncated = strings.ToLower(titleTruncated)
+	newFeedItem.Data.Id = fmt.Sprintf("leon.wtf:%s:%s",
+		newFeedItem.Data.Created.Format("2006-01-02"),
+		titleTruncated,
+	)
+
+	res := database.Con.Create(&newFeedItem)
+	if res.Error != nil {
+		respondError(&w, res.Error, 500)
+		return
+	}
+
+	// update feedCreatedUpdated time
+	feedCreatedUpdated = time.Now()
+
+	respondInstance(&w, fmt.Sprintf("message created with ID '%d'", newFeedItem.ID), 201)
 }
 
 func rssChangeMessageHandler(w http.ResponseWriter, r *http.Request) {
-	type respBody struct {
-		Done                 bool
-		structs.AtomFeedItem // allow all fields to be adjusted (except "Db_Id" of course)
+	// get path paramter
+	vars := mux.Vars(r)
+	id, ok := vars["db_id"]
+	if !ok {
+		respondError(&w, errors.New(`'db_id' is missing`), 400)
+		return
 	}
 
-	// TODO document: can be used to mark message as done
-	respondPlainJSON(&w, []byte(`{"response": "marked as done"}`), 200)
+	// decode response body
+	var respBody struct {
+		Title    string
+		Content  string
+		Resolved *bool
+	}
+	if err := json.NewDecoder(r.Body).Decode(&respBody); err != nil {
+		respondError(&w, err, 400) // decoding errors means malformed request
+		return
+	}
+
+	// retrieve message to be changed from database
+	var messageToChange structs.AtomFeedItemModel
+	// SELECT * FROM AtomFeedItemModels WHERE id = 'id'
+	res := database.Con.First(&messageToChange, id)
+	if res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			respondError(&w, errors.New("message not found"), 404)
+		} else {
+			respondError(&w, res.Error, 500)
+		}
+		return
+	}
+
+	// make changes
+	changed := make([]string, 0)
+	if respBody.Title != "" {
+		messageToChange.Data.Title = respBody.Title
+		changed = append(changed, "Title")
+	}
+	if respBody.Content != "" {
+		messageToChange.Data.Content = respBody.Content
+		changed = append(changed, "Content")
+	}
+	if respBody.Resolved != nil {
+		if *respBody.Resolved {
+			messageToChange.Data.Description = atomDescriptionStatusResolved
+		} else {
+			messageToChange.Data.Description = atomDescriptionStatusUnresolved
+		}
+		changed = append(changed, "Resolved")
+	}
+
+	// update "updated" time
+	messageToChange.Data.Updated = time.Now()
+	changed = append(changed, "Updated")
+
+	// save the modified model back to the database
+	database.Con.Save(&messageToChange)
+
+	// update feedCreatedUpdated time
+	feedCreatedUpdated = time.Now()
+
+	respondInstance(
+		&w,
+		fmt.Sprintf("successfully changed the following attributes for message with ID '%d': %v",
+			messageToChange.ID,
+			"["+strings.Join(changed, ", ")+"]",
+		),
+		200,
+	)
 }
 
 func rssDeleteMessageHandler(w http.ResponseWriter, r *http.Request) {
+	// get path paramter
 	vars := mux.Vars(r)
 	id, ok := vars["db_id"]
 	if !ok {
@@ -113,9 +237,14 @@ func rssDeleteMessageHandler(w http.ResponseWriter, r *http.Request) {
 
 	database.Con.Delete(&messageToDelete)
 
+	// update feedCreatedUpdated time
+	feedCreatedUpdated = time.Now()
+
 	respondInstance(
 		&w,
-		fmt.Sprintf("deleted message '%s'", messageToDelete.Data.Title),
+		fmt.Sprintf("deleted message '%s'",
+			messageToDelete.Data.Title,
+		),
 		200,
 	)
 }
